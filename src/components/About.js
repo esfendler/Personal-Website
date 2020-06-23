e9aa2818985084c7f2d407b730d5eb22dbf11756
Emily <esfendler@gmail.com>
2020-06-23 17:37:16 -0400

    added tech list

--- src/components/About.js
+++ src/components/About.js
@@ -12,9 +12,32 @@ import Emily from '../images/Emily.jpg'
     <img src={Emily} alt="Emily Fendler"/>
     </div>
     <div className="aboutText">
+    <p>
     I am a software engineer with a background working in the data departments of nonprofits. The experience of learning Python at Candid and using this new skill to automate part of my core job responsibilities made me want to code full-time. As a recent graduate of Fullstack Academy's Grace Hopper program, I’m eager to continue following my curiosity in a new role.
+    </p>
+    <p>
+      Here's a list of the technologies I've worked with most recently:
+    </p>
+    <div className="bulletContainer">
+    <div>
+    <ul>
+      <li>JavaScript</li>
+      <li>React</li>
+      <li>Redux</li>
+    </ul>
+    </div>
+    <div>
+    <ul>
+      <li>Node.js</li>
+      <li>Express</li>
+      <li>PostgreSQL</li>
+    </ul>
     </div>
     </div>
+
+
+    </div>
+  </div>
   </div>
 )
   }
